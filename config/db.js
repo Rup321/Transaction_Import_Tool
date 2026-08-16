2b8e1b065b06aa486ab1d3d33df0f0e813b0ec23
Rupali Pawar <rupali.pawar@nimapinfotech.com>
2026-08-16 16:57:20 +0530

    Load models after successful database connection

--- config/db.js
+++ config/db.js
@@ -9,12 +9,15 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     logging: false
 })
 
- 
+const loadModels = () => {
+    require("../src/models")
+}
 
 const dbConnection = async () => {
     try {
         await sequelize.authenticate();
         console.log("Db Connected successfully");
+        loadModels();
 
     }
     catch (err) {
